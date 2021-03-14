ada511a672d341d79e4b046a25a5bff1df2725f5
REDSoftwareRasmus <appdev@redge.se>
2021-03-14 20:01:40 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,20 @@
+from datetime import datetime
+from fastapi import FastAPI
+import starlette
+import graphene
+
+
+
+print(datetime.strftime(datetime.now(), "%Y%m%d"))
+
+def __main__(test: str):
+    
+    print(datetime.strptime(test))
+    print(datetime.strptime())
+
+
+
+
+
+
+
