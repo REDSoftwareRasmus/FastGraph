fb1a0a81b4da881333265e1ed94711b2ace97dfe
REDSoftwareRasmus <appdev@redge.se>
2021-03-15 00:18:02 +0100

    Begin graphene mutation implementation, i.e. CRUD ops

--- src/queries.py
+++ src/queries.py
@@ -1,5 +1,12 @@
 import graphene
 
+from schemas import (
+    UserGrapheneModel,
+    UserGrapheneInputModel
+)
+
+from models import User
+
 
 class Query(graphene.ObjectType):
     hello = graphene.String(name=graphene.String(default_value="Some user"))
@@ -7,5 +14,17 @@ class Query(graphene.ObjectType):
     @staticmethod
     def resolve_hello(parent, info, name):
         return f"Hello {name}"
+
+class CreateUser(graphene.Mutation):
+    class Arguments:
+        user_args = UserGrapheneInputModel()
+
+    Output = UserGrapheneModel
+
+    @staticmethod
+    def mutate(parent, info, user_args):
+        user = User(**user_args.dict())
+        user.save()
+        return user
 
 
